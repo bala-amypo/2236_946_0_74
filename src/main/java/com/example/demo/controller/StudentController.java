ed6833937861a99f9aee46e1d6eb701be57463dc
bala-amypo <bala@amypo.in>
2025-12-17 10:29:50 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -32,7 +32,11 @@ public class StudentController {
          return stser.fetchDataById(id);
     }
     @PutMapping("/updatedata/{id}")
-    public  fetchDataById(@PathVariable int id,@RequestBody Student stu){
+    public  String fetchDataById(@PathVariable int id,@RequestBody Student stu){
+     stu.setId(id);
+     stuser.createData(stu);
+     return "Data updation done!!!";
+
 
     }
    
